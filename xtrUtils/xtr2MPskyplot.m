0686763ee87d8ad159ca498d8af110ddd49cf1f6
spanikp <spanikp@yahoo.com>
2018-09-14 14:56:05 +0200

    Change in input parameters. Added functionality to control colorbar range and export to PNG with defined resolution

--- xtrUtils/xtr2MPskyplot.m
+++ xtrUtils/xtr2MPskyplot.m
@@ -1,4 +1,4 @@
-function xtr2MPskyplot(xtrFileName,MPcode)
+function xtr2MPskyplot(xtrFileName, MPcode, saveFig, options)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function to read Gnut-Anubis XTR output file and make MP skyplot graphs.
 % Process iterates through all available satellite systems (it will
@@ -9,11 +9,42 @@ function xtr2MPskyplot(xtrFileName,MPcode)
 % MPcode - 2-char representation of MP code combination to plot
 %        - values corresponding to RINEX v2 code measurements
 %
+% Optional:
+% saveFig - true/false flag to export plots to PNG file (default: true)
+% options - {colorBarLimits, colorBarTicks, figureResolution}
+%         - default values: colorBarLimits = [0 120];
+%                           colorBarTicks = 0:20:120;
+%
 % Requirements:
 % polarplot3d.m, findGNSTypes.m, dataCell2matrix.m, getNoSatZone.m
 %
-% Peter Spanik, 13.9.2018
+% Peter Spanik, 14.9.2018
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Close all opened figures
+close all
+
+% Check input values
+if nargin == 2
+   saveFig = true;
+   options = {[0, 120], 0:20:120, '200'};
+   if ~ischar(xtrFileName) || ~ischar(MPcode)
+      error('Inputs "xtrFileName" and "MPcode" have to be strings!') 
+   end
+   %xtr2MPskyplot('wecafare-VISO.xtr','C1',false,{[0,150],0:50:150},'150')
+elseif nargin == 3
+    saveFig = logical(saveFig);
+   if ~ischar(xtrFileName) || ~ischar(MPcode) || numel(saveFig) ~= 1 
+      error('Inputs "xtrFileName","MPcode" have to be strings and "saveFig" has to be single value!') 
+   end
+   options = {[0, 120], 0:20:120, '200'};
+elseif nargin == 4
+   if numel(options) ~= 3
+      error('Input variable "limAndTicks" have to be cell of the following form {[1x2 array], [1xn array], [1xn char]}!') 
+   end
+else
+   error('Only 2, 3 or 4 input values are allowed!') 
+end
 
 % File loading
 finp = fopen(xtrFileName,'r');
@@ -26,10 +57,13 @@ data = data(~cellfun(@(c) isempty(c), data));
 % Find indices of Main Chapters (#)
 GNScell = findGNSTypes(data);
 
+% Set custom colormap -> empty bin = white
+myColorMap = colormap(jet); close; % Command colormap open figure!
+myColorMap = [[1,1,1]; myColorMap];
+
 % Satellite's data loading
 for i = 1:length(GNScell)
     % Find position estimate
-    %=XYZGPS 2018-08-14 00:00:00     3933300.9260    1449651.8012    4792338.8227     1.2     0.8     1.5    96     0
     selpos = cellfun(@(c) strcmp(['=XYZ', GNScell{i}],c(1:7)), data);
     postext = char(data(selpos));
     pos = str2num(postext(30:76));
@@ -65,7 +99,6 @@ for i = 1:length(GNScell)
     MP.(GNScell{i}).vals = dataMatrix;
     sel3 = ~isnan(dataMatrix);
     
-    % Transforming common values to vectors
     sel = sel1 & sel2 & sel3;
     ELE.(GNScell{i}).vector = ELE.(GNScell{i}).vals(sel);
     AZI.(GNScell{i}).vector = AZI.(GNScell{i}).vals(sel);
@@ -84,25 +117,24 @@ for i = 1:length(GNScell)
     in = inpolygon(xq,yq,x_edge,y_edge);
     mpg(in) = -1;
     
+    % Create figure
     figure('Position',[300 100 700 480],'NumberTitle', 'off','Resize','off')
-    colorbar_range = [0, 120];
-    polarplot3d(flipud(mpg),'PlotType','surfn','RadialRange',[0 90],'PolarGrid',{6,12},...
-        'GridStyle',':','AxisLocation','surf');
+    polarplot3d(flipud(mpg),'PlotType','surfn','RadialRange',[0 90],'PolarGrid',{6,12},'GridStyle',':','AxisLocation','surf');
     view(90,-90)
     
-    % Set custom colormap -> empty bin = white
-    myColorMap = colormap(jet);
-    myColorMap = [[1,1,1]; myColorMap];
     colormap(myColorMap)
     c = colorbar;
-    c.Limits = [5, 120];
-    c.Ticks = 0:20:120;
+    colLimits = options{1};
+    colLimits(1) = colLimits(1) + 5;
+    c.Limits = colLimits;
+    c.Ticks = options{2};
     c.Position = [c.Position(1)*1.02, c.Position(2)*1.4, 0.8*c.Position(3), c.Position(4)*0.9];
     c.TickDirection = 'in';
     c.LineWidth = 1.1;
     c.FontSize = 10;
+    % Transforming common values to vectors
     
-    caxis([0 120])
+    caxis(options{1})
     ylabel(c,sprintf('%s RMS MP%s value (cm)',GNScell{i},MPcode),'fontsize',10,'fontname','arial')
     axis equal
     axis tight
@@ -110,5 +142,11 @@ for i = 1:length(GNScell)
     hold on
     text(60,0,-100,'30','FontSize',10,'HorizontalAlignment','center','background','w','fontname','arial','FontWeight','bold')
     text(30,0,-100,'60','FontSize',10,'HorizontalAlignment','center','background','w','fontname','arial','FontWeight','bold')
-
+    
+    % Exporting figure
+    if saveFig == true
+       splittedInputName = strsplit(xtrFileName,'.');  
+       figName = [splittedInputName{1}, '_', GNScell{i}, '_MP', MPcode];
+       print(figName,'-dpng',sprintf('-r%s',options{3}))
+    end
 end
